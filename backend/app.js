641374281e6e51a9a53467a28ac78b9ed58e6e09
Nulan Musayev <nurlan_plaz@mil.ru>
2025-04-21 13:36:17 +0200

    mern_place_app

--- backend/app.js
+++ backend/app.js
@@ -49,5 +49,5 @@ app.use( ( error, req, res, next ) =>
 
 mongoose
 	.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kmh9nfm.mongodb.net/X${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
-	.then( () =>app.listen( 5000 ))
+	.then( () =>app.listen(process.env.PORT || 5000 ))
 	.catch(err=>console.log(err));
